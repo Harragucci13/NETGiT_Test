ed3c170910c07f6b0693696c6ad73e085983c66f
Samson Oduor <samson.oduor@strathmore.edu>
2018-09-24 20:20:29 +0300

    initial commit.

---
+++ src/github_test/Github_test.java
@@ -0,0 +1,18 @@
+/*
+ * THIS IS MY TEST PROJECT ON HOW TO USE THE PROJECT
+ * WITH MY GITHUB ACCOUNT
+ */
+package github_test;
+/**
+ * @author keicy
+ */
+public class Github_test {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HELLO JAVA, THIS IS MY GITHUB ACCOUNT");
+    }
+    
+}
